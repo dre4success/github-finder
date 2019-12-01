8370e43cb84b7cf90879cd0f30c3a89dffaf4b0b
dre4success <dre4success@live.com>
2019-12-01 16:27:12 +0100

    using react context API

---
+++ src/context/alert/alertState.js
@@ -0,0 +1,29 @@
+import React, { useReducer } from "react";
+import { AlertContext } from "./alertContext";
+import AlertReducer from "./alertReducer";
+
+import { SET_ALERT, REMOVE_ALERT } from "../types";
+
+export const AlertState = props => {
+  const initialState = null;
+
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+  // set Alert
+  const setAlert = (message, type) => {
+    dispatch({ type: SET_ALERT, payload: { message, type } });
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: null }), 5000);
+  };
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alert: state,
+        setAlert
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
